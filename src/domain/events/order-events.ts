9b2aa93e5b8336fb205ececc77af3f16c5fd582d
guilhermevialle <vialleguilherme@gmail.com>
2025-06-03 18:40:09 -0300

    added last commit

---
+++ src/domain/events/order-events.ts
@@ -0,0 +1,8 @@
+export const ORDER_EVENTS = {
+  EXCHANGE: 'order-events',
+  ROUTING_KEYS: {
+    CREATED: 'order.created',
+    PROCESSING_STARTED: 'order.processing.started',
+    PROCESSING_FINISHED: 'order.processing.finished'
+  }
+}
